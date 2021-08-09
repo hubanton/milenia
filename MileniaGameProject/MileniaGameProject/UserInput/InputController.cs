8ed2010982af304958e017b348e80b7a37bc89c7
Willi <willi.schiebelbein@gmx.de>
2021-08-09 17:09:07 +0200

    walking diagonally is now not faster then walking horizontally or vertically

--- MileniaGameProject/MileniaGameProject/UserInput/InputController.cs
+++ MileniaGameProject/MileniaGameProject/UserInput/InputController.cs
@@ -13,7 +13,7 @@ namespace MileniaGameProject.UserInput
         private Map _map;
 
         //Determines speed of player
-        private int _velocity = 4;
+        private int _velocity = 8;
         
         public InputController(Character character, Map map)
         {
@@ -24,6 +24,8 @@ namespace MileniaGameProject.UserInput
         public void ProcessControls(GameTime gameTime, Map map)
         {
             KeyboardState keyboardState = Keyboard.GetState();
+            int punish = 0;
+            Keys[] keys = keyboardState.GetPressedKeys();
 
 
             bool isWalkUpwardsPressed = keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W);
@@ -34,44 +36,54 @@ namespace MileniaGameProject.UserInput
             bool isWalkRightPressed =
                 _previousKeyboardState.IsKeyDown(Keys.Right) || _previousKeyboardState.IsKeyDown(Keys.D);
 
+            bool isDiagonalUpLeft = isWalkUpwardsPressed && isWalkLeftPressed;
+            bool isDiagonalUpRight = isWalkUpwardsPressed && isWalkRightPressed;
+            bool isDiagonalDownLeft = isWalkDownwardsPressed && isWalkLeftPressed;
+            bool isDiagonalDownRight = isWalkDownwardsPressed && isWalkRightPressed;
+
+            if (isDiagonalUpLeft || isDiagonalUpRight || isDiagonalDownLeft || isDiagonalDownRight)
+            {
+                punish = (int) (_velocity - _velocity * (Math.Sqrt(2) / 2));
+            }
+
             if (isWalkDownwardsPressed &&
                 map.Position.Y <= ((map.MapTexture.Height - Milenia.DefaultHeight)) && _character.Position.Y >= (Milenia.DefaultHeight - _character.CharTexture.Height) / 2)
             {
-                map.Position.Y += _velocity;
+                map.Position.Y += _velocity - punish;
             }
             else if (isWalkDownwardsPressed &&
                      _character.Position.Y <= (Milenia.DefaultHeight - Milenia.PlayerHeight))
             {
-                _character.Position.Y += _velocity;
+                _character.Position.Y += _velocity - punish;
             }
 
             if (isWalkUpwardsPressed && _map.Position.Y >= 0 &&
                 _character.Position.Y <= Milenia.DefaultHeight / 2)
             {
-                _map.Position.Y -= _velocity;
+                _map.Position.Y -= _velocity - punish;
             }
             else if (isWalkUpwardsPressed && _character.Position.Y >= 0)
             {
-                _character.Position.Y -= _velocity;
+                _character.Position.Y -= _velocity - punish;
             }
 
             if (isWalkLeftPressed && _map.Position.X >= 0 && _character.Position.X <= (Milenia.DefaultWidth - Milenia.PlayerWidth) / 2)
             {
-                _map.Position.X -= _velocity;
+                _map.Position.X -= _velocity - punish;
             }
             else if (isWalkLeftPressed && _character.Position.X >= 0)
             {
-                _character.Position.X -= _velocity;
+                _character.Position.X -= _velocity - punish;
             }
 
             if (isWalkRightPressed && _map.Position.X <= (map.MapTexture.Width - Milenia.DefaultWidth) &&
                 _character.Position.X >= (Milenia.DefaultWidth - Milenia.PlayerWidth) / 2)
             {
-                _map.Position.X += _velocity;
+                _map.Position.X += _velocity - punish;
             }
             else if (isWalkRightPressed && _character.Position.X <= (Milenia.DefaultWidth - Milenia.PlayerWidth))
             {
-                _character.Position.X += _velocity;
+                _character.Position.X += _velocity - punish;
             }
 
             _previousKeyboardState = keyboardState;
